9badfb49a15d9d1a21fca0379331927d9b2876ef
Abdulsattar <asattar.md@gmail.com>
2020-03-07 16:38:56 +0530

    Add key to tweets

--- client/src/Tweets.js
+++ client/src/Tweets.js
@@ -4,7 +4,7 @@ import { Tweet } from "./Tweet";
 function Tweets(props) {
   const tweetsHtml = [];
   for (var i = 0; i < props.tweets.length; i++) {
-    tweetsHtml.push(<Tweet author={props.tweets[i].author} tweet={props.tweets[i].tweet} />);
+    tweetsHtml.push(<Tweet key={props.tweets[i].id} author={props.tweets[i].author} tweet={props.tweets[i].tweet} />);
   }
   return <div className="tweets">{tweetsHtml}</div>;
 }
